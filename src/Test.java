5fe41ac1896e78095988d75c72f3b31b76e8c2a3
S T Pell <pellsamuel@gmail.com>
2017-03-26 12:58:42 +1300

    Test Documentation

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,8 @@
-
+/**
+ * Test method for initial GIT commit
+ * @author Samuel Pell
+ *
+ */
 public class Test {
 	public static void main(String[] args){
 		System.out.println("Does this work? - Probably yes if you can see this question");
