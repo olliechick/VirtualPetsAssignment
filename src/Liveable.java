ff25f8c5efccddb7f0b51018a7e9c5840baba4b5
olliechick <olliechick@gmail.com>
2017-04-11 20:20:23 +1200

    add javadoc comments for methods in Liveable

--- src/Liveable.java
+++ src/Liveable.java
@@ -5,12 +5,35 @@
  */
 public interface Liveable {
 	
+	/**
+	 * The pet plays with the toy
+	 * @param toy the toy the pet plays with
+	 */
 	void play(Toy toy);
+	/**
+	 * The pet sleeps
+	 */
 	void sleep();
+	/**
+	 * The pet goes toilet
+	 */
 	void goToilet();
+	/**
+	 * The pet eats food
+	 * @param food the food the pet eats
+	 */
 	void feed(Food food);
+	/**
+	 * The pet misbehaves
+	 */
 	void misbehave();
+	/**
+	 * The pet is sick
+	 */
 	void beSick();
+	/**
+	 * The pet dies
+	 */
 	void die();
 
 }
