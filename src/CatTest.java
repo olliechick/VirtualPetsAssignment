9abb09f806ff287913ec1a165f2be0527caabdde
S T Pell <pellsamuel@gmail.com>
2017-04-12 09:42:26 +1200

    Implemented a more rigourous play test for cat

--- src/CatTest.java
+++ src/CatTest.java
@@ -12,18 +12,32 @@ import org.junit.Before;
 public class CatTest {
 	
 	private Cat myCat;
+	private Cat myOtherCat;
 	private Toy myToy; 
+	private Food myFood;
 	
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception{		
 		myCat = new Cat();
+		myOtherCat = new Cat();
 		
-		//initialise myToy
-		myToy = new Toy("scratching post", "", 1, 2);
-		//add values for happiness increase to myToy
 		String[] species = {myCat.getSpecies()};
 		int[] values = {25};
-		myToy.setHappinessIncrease(species, values); 
+		
+		//initialise myToy and myFood
+		myToy = new Toy("scratching post", "", 1, 3);
+		myFood = new Food("Meatloaf", "", 1, 3);
+		
+		//add values for happiness increase to myToy
+		myToy.setHappinessIncrease(species, values);
+		//add values for health increase to myFood
+		myFood.setHealthIncrease(species, values);
+		
+		//Set myOtherCat's attributes to extremes
+		myOtherCat.increaseMischievousness(100);
+		myOtherCat.increaseHappiness(-100);
+		myOtherCat.increaseFatigue(100);
+		myOtherCat.increaseHunger(100);
 	}
 
 	@Test
@@ -39,24 +53,34 @@ public class CatTest {
 		int initialFatigue = myCat.getFatigue();
 		int initialMischievousness = myCat.getMischievousness();
 		int initialHunger = myCat.getHunger();
+	
+		int otherInitialHappiness = myOtherCat.getHappiness();
+		int otherInitialFatigue = myOtherCat.getFatigue();
+		int otherInitialMischievousness = myOtherCat.getMischievousness();
+		int otherInitialHunger = myOtherCat.getHunger();
 		
 		//play with the toy
 		myCat.play(myToy);
+		myOtherCat.play(myToy);
 		
 		//check new values are as expected
-		assertTrue(initialDurability  >myToy.getDurability());
+		assertTrue(initialDurability  > myToy.getDurability());
 		
 		//check if initial happiness is lower than new happines or they are both maxed out (100)
-		assertTrue(initialHappiness < myCat.getHappiness() || (initialHappiness == 100 && myCat.getHappiness() == 100));
+		assertTrue((initialHappiness == 100 && myCat.getHappiness() == 100));
+		assertTrue(otherInitialHappiness < myOtherCat.getHappiness());
 		
 		//check if initial fatigue is lower than new fatigue or they are both 100
-		assertTrue(initialFatigue<myCat.getFatigue() || (initialFatigue == 100 && myCat.getFatigue() == 100));
+		assertTrue(initialFatigue < myCat.getFatigue());
+		assertTrue(otherInitialFatigue == 100 && myOtherCat.getFatigue() == 100);
 		
 		//check if initial mischeviousness is lower than new mischeviousness or they are both 0
-		assertTrue(initialMischievousness>myCat.getMischievousness() || (initialMischievousness == 0 && myCat.getMischievousness() == 0));
+		assertTrue(initialMischievousness == 0 && myCat.getMischievousness() == 0);
+		assertTrue(otherInitialMischievousness > myOtherCat.getMischievousness());
 		
 		//check if animal is hungrier than before or that hunger was initially maxed at 100 
-		assertTrue(initialHunger<myCat.getHunger() || (initialHunger == 100 && myCat.getHunger() == 100) );
+		assertTrue(initialHunger < myCat.getHunger());
+		assertTrue(otherInitialHunger == 100 && myOtherCat.getHunger() == 100);
 	}
 
 	@Test
