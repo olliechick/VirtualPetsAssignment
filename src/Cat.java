fb4371b4c8561c10de1e6a74d3dbcef1eba5c1a8
olliechick <olliechick@gmail.com>
2017-04-11 18:05:48 +1200

    starting cat

---
+++ src/Cat.java
@@ -0,0 +1,51 @@
+/**
+ * Cat, a type of pet
+ * @author Ollie Chick
+ *
+ */
+public class Cat extends Pet implements Liveable {
+
+	@Override
+	public void play(Toy toy) {
+		currentDurability = toy.getDurability();
+		newDu
+
+	}
+
+	@Override
+	public void sleep() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void goToilet() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void feed(Food food) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void misbehave() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void beSick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
