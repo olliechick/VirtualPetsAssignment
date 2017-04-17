86d7a043d3bc6d0149e3cb2add30847f69258bee
S T Pell <pellsamuel@gmail.com>
2017-04-18 07:59:59 +1200

    Further implementation of GameEnvironment, condesing code, correcting comments and queries

--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.IOException;
 
 /**
  * Launching off point for Virtual Pets game.
@@ -18,7 +19,7 @@ public class GameEnvironment {
 	
 	
 	/**
-	 * Gets the number of days the game is to run for
+	 * Gets the number of days the game is to run for.
 	 * @return Number of days to run game for.
 	 */
 	private int getNumberOfDays(){
@@ -44,28 +45,30 @@ public class GameEnvironment {
 	}
 	
 	/**
-	 * @return Number of players.
+	 * Used to ask the user the number of pets/players desired (between 1 and 3 inclusive).
+	 * @param query Query to pose to user.
+	 * @return Number of required pets or players.
 	 */
-	private int getNumberOfPlayers(){
-		Integer numPlayers = null;
+	private int getNumberRequired(String query){
+		Integer numReq = null;
 		
 		do{
-			System.out.print("How many players? (1-3)    ");
+			System.out.print(query);
 			System.out.flush();
 			String userInput = inputReader.next();
 			try{
-				numPlayers = Integer.parseInt(userInput);
-				if (numPlayers < 1 || numPlayers > 3){ 
+				numReq = Integer.parseInt(userInput);
+				if (numReq< 1 || numReq > 3){ 
 					//if number is out of bounds 1-3 throw exception
-					numPlayers = null;
+					numReq = null;
 					throw new NumberFormatException();
 				}
 			}catch (NumberFormatException exception){
 				System.out.println("Please enter an integer between 1 and 3 inclusive.");
 			}
-		}while (numPlayers == null);
+		}while (numReq == null);
 		
-		return numPlayers;
+		return numReq;
 	}
 	
 	/**
@@ -87,42 +90,110 @@ public class GameEnvironment {
 	}
 	
 	/**
+	 * Get the name of a player or pet.
+	 * @param query Query to pose to user.
+	 * @return name of player or pet.
+	 */
+	private String getName(String query){
+		String name = null;
+		
+		do{
+			System.out.print(query);
+			System.out.flush();
+			name = inputReader.next();
+			if (nameTaken(name)){ //if name is already taken
+				name = null;
+				System.out.println("Duplicate names are not allowed.");
+			}
+		} while (name == null);
+		
+		nameList.add(name);
+		return name;
+	}
+	
+	/**
 	 * Sets the name for a new player.
 	 * @param newPlayer Player object to set name for.
 	 */
 	private void setPlayerName(Player newPlayer){
-		String playerName = null;
-		
+		try{
+			newPlayer.setName(getName("Player name: "));	
+		}catch (IllegalArgumentException exception){
+			System.out.println("Unknown error. Please try again.");
+		}
+	}
+	
+	//TODO: Ask Ollie how to handle IOException from Pet class
+	/**
+	 * Creates a pet object based on user input
+	 * @return pet of species desired by player
+	 * @throws IOException because it isn't handled at lower levels
+	 */
+	private Pet createPetSpecies() throws IOException{
+		Pet newPet = new Cat();
+		String choice;
+		//Get pet species
 		do{
-			System.out.print("Player Name:    ");
+			System.out.println("1- Alpaca\n2- Cat\n3- Dog\n4- Goat\n5- Horse\n6- Polar Bear");
+			System.out.print("What Pet would you like?    ");
 			System.out.flush();
-			playerName = inputReader.next();
-			
-			try{
-				if (nameTaken(playerName)){ //if name is already taken
-					playerName = null;
-					throw new IllegalArgumentException("Reserved name");
-				}else{ //otherwise just set the name
-					newPlayer.setName(playerName);
-					nameList.add(playerName);
-				}
-			}catch (IllegalArgumentException exception){
-				
-				if (exception.getMessage() == "Reserved name")
-					System.out.println("Duplicate names are not allowed.");
-				else
-					System.out.println("Unknown error. Please try again.");
+
+			choice = inputReader.next();
+
+			switch(choice.toLowerCase()){ //TODO: When all animals implemented add appropriate calls
+			case "1":
+			case "alpaca":
+				System.out.println("Creating " + choice);
+				break;
+
+			case "2":
+			case "cat": 
+				System.out.println("Creating " + choice);
+				newPet = new Cat();
+				break;
+
+			case "3":
+			case "dog":
+				System.out.println("Creating " + choice);
+				newPet = new Dog();
+				break;
+
+			case "4":
+			case "goat":
+
+			case "5":
+			case "horse":
+
+			case  "6":
+			case "polar bear":
+				System.out.println("Creating " + choice);
+				break;
+
+			default:
+				System.out.println(choice + " is not a valid option. Please enter one of the below choices.");
+				choice = null;
+				break;
 			}
-		} while (playerName == null);
+		}while (choice == null);
+		
+		return newPet;
 	}
 	
-	private int numberOfPets(){
-		//TODO: Add method to get number of pets desired
-		return numPets;
-	}
 	
-	private Pet createPet(){
-		//TODO: Add method to create a pet
+	/**
+	 * Creates a pet for a player
+	 * @return Pet player has designed
+	 * @throws IOException
+	 */
+	private Pet createPet() throws IOException{
+		Pet newPet = createPetSpecies();
+		
+		try{
+			newPet.setName(getName("Pet name: "));	
+		}catch (IllegalArgumentException exception){
+			System.out.println("Unknown error. Please try again.");
+		}
+		//TODO: Finish implementing createPet
 		return newPet;
 	}
 	
@@ -130,12 +201,16 @@ public class GameEnvironment {
 	 * Creates a new player
 	 * @return A fully created player
 	 */
-	private Player createPlayer(){
+	private Player createPlayer() throws IOException{
 		Player newPlayer = new Player();
 		setPlayerName(newPlayer);
-		int numPets = numberOfPets();
+		int numPets = getNumberRequired("How many pets for player " + newPlayer.getName() + "?    ");
+		
+		ArrayList<Pet> playerPetList = newPlayer.getPetList();
+		Pet newPet;
 		for (int i = 0; i < numPets; i++){
-			newPlayer.getPetList().add(createPet());
+			newPet = createPet();
+			playerPetList.add(newPet);
 		}
 		return newPlayer;
 	}
@@ -147,15 +222,16 @@ public class GameEnvironment {
 		inputReader.close();
 	}
 	
+	
 	/**
 	 * Performs all setup for the game.
 	 * 
 	 * Creates players, their pets, and a prototype of each Toy and Food.
 	 */
-	private void setup(){
+	private void setup() throws IOException{
 		numberOfDays = getNumberOfDays();
 		dayNumber = 1;
-		int numPlayers = getNumberOfPlayers();
+		int numPlayers = getNumberRequired("How many players?    ");
 		playerList = new Player[numPlayers];
 	
 		for (int i = 0; i < numPlayers; i++){
@@ -168,7 +244,7 @@ public class GameEnvironment {
 	 * Main entry point.
 	 * @param args Arguments - don't really have many of them
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		//Testing if setup works
 		GameEnvironment mainGame = new GameEnvironment();
 		mainGame.setup();
