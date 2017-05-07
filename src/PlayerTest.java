a720e50522a23c715524fc16852b44ed03de9c91
olliechick <olliechick@gmail.com>
2017-05-08 11:00:33 +1200

    start PlayerTest

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -4,14 +4,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerTest {
+	String name1;
+	String name2;
+	Player player1;
 
 	@Before
 	public void setUp() throws Exception {
+		name1 = "Harry";
+		name2 = "Rex";
 	}
 
 	@Test
 	public void testPlayer() {
-		fail("Not yet implemented");
+		player1 = new Player();
+		assert(player1.getBalance()>0);
+		assertTrue(player1.getPetList().isEmpty());
+		//TODO here down
+
 	}
 
 	@Test
