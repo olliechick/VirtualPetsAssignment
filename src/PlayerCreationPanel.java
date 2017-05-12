16f4b9b513bad81fbc87e0aac253e9fbcf517d3a
S T Pell <pellsamuel@gmail.com>
2017-05-13 10:59:49 +1200

    Fixed misc, added a button

--- src/PlayerCreationPanel.java
+++ src/PlayerCreationPanel.java
@@ -3,6 +3,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 /**
  * Player creation panel
@@ -18,24 +19,24 @@ public class PlayerCreationPanel extends JPanel{
 		setLayout(null);
 		
 		JLabel lblPlayerName = new JLabel("Player Name:");
-		lblPlayerName.setBounds(10, 14, 70, 14);
+		lblPlayerName.setBounds(10, 14, 89, 14);
 		add(lblPlayerName);
 		
 		textField = new JTextField();
-		textField.setBounds(76, 11, 123, 20);
+		textField.setBounds(88, 11, 123, 20);
 		add(textField);
 		textField.setColumns(10);
 		
 		JCheckBox checkBox = new JCheckBox("1");
-		checkBox.setBounds(56, 57, 43, 23);
+		checkBox.setBounds(56, 75, 43, 23);
 		add(checkBox);
 		
 		JCheckBox checkBox_1 = new JCheckBox("2");
-		checkBox_1.setBounds(194, 57, 43, 23);
+		checkBox_1.setBounds(194, 75, 43, 23);
 		add(checkBox_1);
 		
 		JCheckBox checkBox_2 = new JCheckBox("3");
-		checkBox_2.setBounds(339, 53, 43, 31);
+		checkBox_2.setBounds(339, 71, 43, 31);
 		add(checkBox_2);
 		
 		JLabel lblNumberOfPets = new JLabel("Number of Pets:");
@@ -43,16 +44,20 @@ public class PlayerCreationPanel extends JPanel{
 		add(lblNumberOfPets);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 87, 123, 202);
+		panel.setBounds(10, 105, 123, 202);
 		add(panel);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(143, 87, 141, 202);
+		panel_1.setBounds(143, 105, 141, 202);
 		add(panel_1);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(294, 87, 116, 202);
+		panel_2.setBounds(294, 105, 116, 202);
 		add(panel_2);
+		
+		JButton btnNext = new JButton("Next");
+		btnNext.setBounds(321, 316, 89, 23);
+		add(btnNext);
 
 	}
 	
@@ -63,7 +68,7 @@ public class PlayerCreationPanel extends JPanel{
 	public static void main(String[] args){
 		JFrame myFrame = new JFrame();
 		
-		myFrame.setBounds(0, 0, 400, 226);
+		myFrame.setBounds(0, 0, 450, 400);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.getContentPane().setLayout(null);
 		
@@ -72,7 +77,7 @@ public class PlayerCreationPanel extends JPanel{
 		myFrame.getContentPane().add(myPanel);
 		myPanel.setVisible(true);
 		
-		myPanel.setSize(400, 226);
+		myPanel.setSize(420, 350);
 		
 		myFrame.setVisible(true);
 	}
