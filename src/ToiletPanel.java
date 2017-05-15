d146f4094778c6ea8f1471606de05bbe906aec9b
S T Pell <pellsamuel@gmail.com>
2017-05-16 07:32:23 +1200

    Added toilet screen

---
+++ src/ToiletPanel.java
@@ -0,0 +1,28 @@
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ToiletPanel extends JPanel {
+
+    /**
+     * Create the panel.
+     */
+    public ToiletPanel() {
+        setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("New label");
+        lblNewLabel.setBounds(362, 90, 46, 14);
+        add(lblNewLabel);
+        
+        JButton btnGoToilet = new JButton("Go Toilet");
+        btnGoToilet.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+            }
+        });
+        btnGoToilet.setBounds(340, 349, 89, 23);
+        add(btnGoToilet);
+
+    }
+}
