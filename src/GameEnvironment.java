274df2a290877bae543efe7010ef770af0a3e9a5
S T Pell <pellsamuel@gmail.com>
2017-05-16 16:21:29 +1200

    Jarring days work

--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 
 
 /**
--- src/Pet.java
+++ src/Pet.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 
 /**
  * A class for pets.
