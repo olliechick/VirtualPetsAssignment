ef83ea0329df2e2c2cd130dd0dd35353990e81ea
S T Pell <pellsamuel@gmail.com>
2017-05-17 14:15:49 +1200

    Merging goat

--- src/virtualpets/Goat.java
+++ src/virtualpets/Goat.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:src/Goat.java
-=======
 package virtualpets;
-import java.io.IOException;
 
->>>>>>> 9baffa4a13e106ed33a47f2a01d52968b2c7151d:src/virtualpets/Goat.java
 /**
  * Goat, a type of pet.
  * @author Ollie Chick
