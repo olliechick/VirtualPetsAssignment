0b1e25d119f9d900bd0593d2b3a594fd3ab6c977
S T Pell <pellsamuel@gmail.com>
2017-05-17 14:12:09 +1200

    Merging polar bear

--- src/virtualpets/PolarBear.java
+++ src/virtualpets/PolarBear.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD:src/PolarBear.java
-=======
 package virtualpets;
 import java.io.IOException;
 
->>>>>>> 9baffa4a13e106ed33a47f2a01d52968b2c7151d:src/virtualpets/PolarBear.java
 /**
  * Polar bear, a type of pet.
  * @author Ollie Chick
