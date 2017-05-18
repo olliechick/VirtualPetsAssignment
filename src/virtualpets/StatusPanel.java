512f5b1ef9019bc9a76cad27d3631f059e06b672
S T Pell <pellsamuel@gmail.com>
2017-05-18 14:19:14 +1200

    Sam's misadventure in GUI

--- src/virtualpets/StatusPanel.java
+++ src/virtualpets/StatusPanel.java
@@ -1,7 +1,6 @@
 package virtualpets;
 import javax.swing.JPanel;
 import java.awt.Color;
-import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
