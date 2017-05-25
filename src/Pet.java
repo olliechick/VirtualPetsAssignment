95973221d1a871d405d2cefdbe3be456ae34a1fe
Samuel Pell <pellsamuel@gmail.com>
2017-05-26 06:19:32 +1200

    Fixing imports

--- src/Pet.java
+++ src/Pet.java
@@ -1,5 +1,9 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
  * A class for pets.
